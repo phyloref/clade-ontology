f66bf7a8f46817263da8ac2988386ec1611bfa94
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-06-25 15:26:31 -0400

    Created a private directory that is ignored by Git but searched by conftest.py.

---
+++ private/README.md
@@ -0,0 +1,6 @@
+# Directory for private phyloreferences
+
+Some phyloreferences have not yet been published. They should be stored in this
+directory and NOT added to the Github repository. JSON files with any name in 
+subdirectories of this directory (e.g. `private/from_dropbox/mammalia_41.json`) 
+will be tested with the test suite when it is run.
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,10 @@ import pytest
 import os
 import fnmatch
 
-phylorefs_path = "phyx"
+phyloref_paths = [
+    "phyx",
+    "private"
+]
 
 def pytest_generate_tests(metafunc):
     """ 
@@ -16,30 +19,31 @@ def pytest_generate_tests(metafunc):
     curated files to read.
     """
 
-    dirs = [d for d in os.listdir(phylorefs_path) if 
-        os.path.isdir(phylorefs_path + "/" + d) and 
-        d[0] != '.' and 
-            # Ignore Unix hidden folders
-        d != 'lib' and
-            # Ignore the 'lib' directory
-        os.path.isfile(phylorefs_path + "/" + d + "/paper.json")
-            # Only include directories containing 'paper.json'
-    ]
+    for path in phyloref_paths:
+        dirs.extend([d for d in os.listdir(path) if 
+            os.path.isdir(path + "/" + d) and 
+            d[0] != '.' and 
+                # Ignore Unix hidden folders
+            d != 'lib' and
+                # Ignore the 'lib' directory
+            os.path.isfile(path + "/" + d + "/paper.json")
+                # Only include directories containing 'paper.json'
+        ])
 
     if "paper_json" in metafunc.fixturenames:
         metafunc.parametrize(
             "paper_json",
-            [phylorefs_path + "/" + path + "/paper.json" for path in dirs]
+            [path + "/" + path + "/paper.json" for path in dirs]
         )
 
     if "paper_as_owl_json" in metafunc.fixturenames:
         metafunc.parametrize(
             "paper_as_owl_json",
-            [phylorefs_path + "/" + path + "/paper_as_owl.json" for path in dirs]
+            [path + "/" + path + "/paper_as_owl.json" for path in dirs]
         )
     
     if "paper_owl" in metafunc.fixturenames:
         metafunc.parametrize(
             "paper_owl",
-            [phylorefs_path + "/" + path + "/paper.owl" for path in dirs]
+            [path + "/" + path + "/paper.owl" for path in dirs]
         )
