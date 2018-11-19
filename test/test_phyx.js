7830cefa758424580b3c350f9ab4f8f88bb85d91
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-11-19 15:40:25 -0500

    Fixed git-crypt detection.

--- test/test_phyx.js
+++ test/test_phyx.js
@@ -12,7 +12,7 @@ const assert = chai.assert;
 
 /*
  * Returns a list of PHYX files that we can test in the provided directory.
- * 
+ *
  * We use a simple flatMap to search for these files. Since flatMap isn't in
  * Node.js yet, I use a map to recursively find the files and then flatten it
  * using reduce() and concat().
@@ -44,16 +44,13 @@ describe('Test PHYX files in repository', function() {
     findPHYXFiles(BASE_DIR).forEach(function(filename) {
         describe('PHYX file: ' + filename, function() {
             const stats = fs.lstatSync(filename);
-            
+
             it('is not empty', function() {
                 assert.notEqual(stats["size"], 0);
             });
 
-            const readStream = fs.createReadStream(filename, {start: 0, end: 9});
-            var data;
-            while(null !== (data = readStream.read(9)));
-
-            if(data === "\x00GITCRYPT") {
+            const data = fs.readFileSync(filename).slice(0, 9);
+            if(data.equals(Buffer.from("\x00GITCRYPT"))) {
                 it.skip('is git-crypt encrypted');
                 return;
             }
@@ -65,4 +62,3 @@ describe('Test PHYX files in repository', function() {
 
 
 //  2. Skip files that start with "\x00GITCRYPT", which means they are Git encrypted.
-
