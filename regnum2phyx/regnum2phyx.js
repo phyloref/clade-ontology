f2d9be06c194ea1e2e91a787efdd5d0dbcf09a27
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-03-01 18:40:28 -0500

    First draft of a regnum2phyx script.

---
+++ regnum2phyx/regnum2phyx.js
@@ -0,0 +1,97 @@
+/*
+ * regnum2phyx.js: A tool for converting PhyloRegnum database dumps to Phyx files.
+ *
+ * Synopsis: regnum2phyx.js [JSON file to process] -o [directory to write files to]
+ *
+ * PhyloRegnum can be accessed at http://app.phyloregnum.org
+ */
+
+// Load necessary modules.
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+const { has, keys } = require('lodash');
+
+// Read command-line arguments.
+const argv = yargs
+  .usage('Usage: $0 <JSON file to process> -o <directory to write files to>')
+  .demandCommand(1, 1) // Make sure there's one and only one file to convert.
+  .option('output-dir', {
+    alias: 'o',
+    describe: 'Directory to write Phyx files to',
+  })
+  .demandOption(['o'])
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+// Read the database dump.
+const dump = JSON.parse(fs.readFileSync(argv._[0], 'utf8'));
+
+// This dump consists of multiple named phyloreferences, each of which should be
+// written out to a separate file.
+const phyxProduced = {};
+let countErrors = 0;
+
+dump.forEach((entry) => {
+  const phylorefLabel = entry.name.trim();
+
+  if (has(phyxProduced, phylorefLabel)) {
+    process.stderr.write(`Duplicate phyloreference label '${phylorefLabel}', skipping.`);
+    countErrors += 1;
+    return;
+  }
+
+  // Prepare and fill a simple Phyx file template.
+  const phylorefTemplate = {
+    label: phylorefLabel,
+    cladeDefinition: (entry.definition || '').trim(),
+    internalSpecifiers: [],
+    externalSpecifiers: [],
+  };
+
+  (entry.specifiers || []).forEach((specifier) => {
+    const kind = specifier.specifier_kind || '';
+    let addTo = [];
+    if (kind.startsWith('internal')) addTo = phylorefTemplate.internalSpecifiers;
+    else if (kind.startsWith('external')) addTo = phylorefTemplate.externalSpecifiers;
+    else {
+      process.stderr.write(`Unknown specifier type: '${kind}' for phyloreference '${phylorefLabel}'.\n`);
+      countErrors += 1;
+    }
+
+    const scname = `${specifier.specifier_name || ''} ${specifier.displayAuths || ''}, ${specifier.specifier_year || ''} [${specifier.specifier_code || ''}]`;
+    const specifierTemplate = {
+      verbatimSpecifier: scname,
+      referencesTaxonomicUnits: [{
+        scientificNames: [{
+          scientificName: scname,
+        }],
+      }],
+    };
+
+    addTo.push(specifierTemplate);
+  });
+
+  // Prepare a simple Phyx file template.
+  const phyxTemplate = {
+    '@context': 'http://www.phyloref.org/phyx.js/context/v0.1.0/phyx.json',
+    phylogenies: [],
+    phylorefs: [phylorefTemplate],
+    debug: entry, // To help with debugging, dump the entire entry into the Phyx file.
+  };
+
+  // Write out Phyx file.
+  const phyxFilename = path.join(argv.outputDir, `${phylorefLabel}.json`);
+  fs.writeFileSync(phyxFilename, JSON.stringify(phyxTemplate, null, 4));
+
+  // Save for later use if needed.
+  phyxProduced[phylorefLabel] = entry;
+});
+
+if (countErrors > 0) {
+  process.stderr.write(`${countErrors} errors occurred while processing database dump.\n`);
+  process.exit(1);
+} else {
+  process.stdout.write(`${keys(phyxProduced).length} Phyx files produced successfully.\n`);
+}
