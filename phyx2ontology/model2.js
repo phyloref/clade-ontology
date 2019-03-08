46e2eb291d3ad366edcf5ad34a3af98f50388203
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-03-08 13:14:24 -0500

    Added more documentation.

--- phyx2ontology/model2.js
+++ phyx2ontology/model2.js
@@ -86,38 +86,46 @@ function createAdditionalClass(jsonld, internalSpecifiers, externalSpecifiers, e
   // exists for this set of internal and external specifiers, we just return that
   // instead of creating a new one.
   //
-  // For some reason this acts strange if equivalentClass is a string, so instead
-  // I've made it into a function here.
+  // TODO For some reason this acts strange if equivalentClass is a string, so instead
+  // I've made it into a function here. To be fixed.
 
   if (internalSpecifiers.length === 0) throw new Error('Cannot create additional class without any internal specifiers');
   if (internalSpecifiers.length === 1 && externalSpecifiers.length === 0) throw new Error('Cannot create additional class with a single internal specifiers and no external specifiers');
 
+  /* Come up with a label that represents this additional class. */
+
+  // Start with the internal specifiers, concatenated with '&'.
+  const internalSpecifierLabel = internalSpecifiers
+    .map(i => convertTUtoRestriction(i)[0].someValuesFrom.intersectionOf[1].hasValue || '(error)')
+    .sort()
+    .join(' & ');
+  let additionalClassLabel = `(${internalSpecifierLabel}`;
+
+  if (externalSpecifiers.length === 0) {
+    additionalClassLabel += ')';
+  } else {
+    // Add the external specifiers, concatenated with 'V'.
+    const externalSpecifierLabel = externalSpecifiers
+      .map(i => convertTUtoRestriction(i)[0].someValuesFrom.intersectionOf[1].hasValue || '(error)')
+      .sort()
+      .join(' V ');
+    additionalClassLabel += ` ~ ${externalSpecifierLabel})`;
+  }
+
+  // process.stderr.write(`Additional class label: ${additionalClassLabel}\n`);
+
   // TODO We need to replace this with an actual object-based comparison,
   // rather than trusting the labels to tell us everything.
-  const externalSpecifierLabel = ` ~ ${externalSpecifiers
-    .map(i => convertTUtoRestriction(i)[0].someValuesFrom.intersectionOf[1].hasValue || '(error)')
-    .sort()
-    .join(' V ')}`;
-
-  // Add the internal specifiers to this.
-  const additionalClassLabel = `(${internalSpecifiers
-    .map(i => convertTUtoRestriction(i)[0].someValuesFrom.intersectionOf[1].hasValue || '(error)')
-    .sort()
-    .join(' & ')
-  }${externalSpecifiers.length > 0 ? externalSpecifierLabel : ''
-  })`;
-
-  process.stderr.write(`Additional class label: ${additionalClassLabel}\n`);
-
   if (has(additionalClassesByLabel, additionalClassLabel)) {
-    process.stderr.write(`Found additional class with id: ${additionalClassesByLabel[additionalClassLabel]['@id']}\n`);
+    // If we see the same label again, return the previously defined additional class.
     return { '@id': additionalClassesByLabel[additionalClassLabel]['@id'] };
   }
 
+  // Create a new additional class for this set of internal and external specifiers.
   additionalClassCount += 1;
   const additionalClass = {};
   additionalClass['@id'] = `${jsonld['@id']}_additional${additionalClassCount}`;
-  process.stderr.write(`Creating new additionalClass with id: ${additionalClass['@id']}`);
+  // process.stderr.write(`Creating new additionalClass with id: ${additionalClass['@id']}`);
 
   additionalClass['@type'] = 'owl:Class';
   additionalClass.subClassOf = (
@@ -132,6 +140,7 @@ function createAdditionalClass(jsonld, internalSpecifiers, externalSpecifiers, e
   return { '@id': additionalClass['@id'] };
 }
 
+/* Return an OWL restriction for inclusion of a single taxonomic unit. */
 function getIncludesRestrictionForTU(tu) {
   return {
     '@type': 'owl:Restriction',
@@ -140,8 +149,11 @@ function getIncludesRestrictionForTU(tu) {
   };
 }
 
-
-function getMRCA2Expression(tu1, tu2) {
+/*
+ * Return an OWL restriction for the most recent common ancestor (MRCA)
+ * of two taxonomic units.
+ */
+function getMRCARestrictionOfTwoTUs(tu1, tu2) {
   return {
     '@type': 'owl:Restriction',
     onProperty: 'obo:CDAO_0000149', // cdao:has_Child
@@ -159,25 +171,39 @@ function getMRCA2Expression(tu1, tu2) {
   };
 }
 
-
+/*
+ * Create an OWL restriction for a phyloreference made up entirely of internal
+ * specifiers.
+ *  - jsonld: the JSON-LD representation of this phyloreference in model 1.0.
+ *    We mainly use this to access the '@id' and internal and external specifiers.
+ *  - remainingInternals: all internal specifiers that have not yet been selected.
+ *  - selected: internal specifiers have been seen selected. This should initially
+ *    be [], and will be filled in when this method calls itself recursively.
+ *
+ * This method works like this:
+ *  1. We have several special cases: we fail if 0 or 1 specifiers are
+ *     provided, and we have a special representation for 2 specifiers.
+ *  2. Create an expression for the currently selected specifiers. This expression
+ *     is in the form:
+ *       has_Child some (excludes_lineage_to some [remaining specifiers] and [selected specifiers])
+ *     We generate the expressions for remaining specifiers and selected specifiers by calling
+ *     this method recursively.
+ *  3. Finally, we select another internal from the remainingInternals and generate an
+ *     expression for that selection by calling this method recursively. We stop doing
+ *     this once we've selected as many internals as are remaining, since at that point
+ *     we will have tried every possible combination of internals.
+ *
+ * This ensures that every sequence of internal specifiers are tested.
+ */
 function createClassExpressionsForInternals(jsonld, remainingInternals, selected) {
-  // Create a class expression for a phyloref made up entirely of internal specifiers.
-  //  - additionalClasses: used to store additional classes as needed.
-  //  - remainingInternals: taxonomic units remaining to be included.
-  //  - selected: taxonomic units that have been selected already.
-
-  // This algorithm works like this:
-  //  - 1. We start with everything remaining and nothing selected.
-  //  - 2. We recurse into this method, moving everything in remaining into selected one by one.
-  //    - Think of it as a tree: the root node selects each internal once, and then each child node
-  //      selects one additional item from remaining, and so on.
-  process.stderr.write(`@id [${jsonld['@id']}] Remaining internals: ${remainingInternals.length}, selected: ${selected.length}\n`);
+  // process.stderr.write(`@id [${jsonld['@id']}] Remaining internals:
+  // ${remainingInternals.length}, selected: ${selected.length}\n`);
 
   // Quick special case: if we have two 'remainingInternals' and zero selecteds,
   // we can just return the MRCA for two internal specifiers.
   if (selected.length === 0) {
     if (remainingInternals.length === 2) {
-      return [getMRCA2Expression(remainingInternals[0], remainingInternals[1])];
+      return [getMRCARestrictionOfTwoTUs(remainingInternals[0], remainingInternals[1])];
     } if (remainingInternals.length === 1) {
       throw new Error('Cannot determine class expression for a single specifier');
     } else if (remainingInternals.length === 0) {
@@ -192,7 +218,10 @@ function createClassExpressionsForInternals(jsonld, remainingInternals, selected
     if (remainingInternals.length === 1) {
       remainingInternalsExpr = getIncludesRestrictionForTU(remainingInternals[0]);
     } else if (remainingInternals.length === 2) {
-      remainingInternalsExpr = getMRCA2Expression(remainingInternals[0], remainingInternals[1]);
+      remainingInternalsExpr = getMRCARestrictionOfTwoTUs(
+        remainingInternals[0],
+        remainingInternals[1]
+      );
     } else {
       remainingInternalsExpr = createAdditionalClass(
         jsonld,
@@ -206,7 +235,7 @@ function createClassExpressionsForInternals(jsonld, remainingInternals, selected
     if (selected.length === 1) {
       selectedExpr = getIncludesRestrictionForTU(selected[0]);
     } else if (selected.length === 2) {
-      selectedExpr = getMRCA2Expression(selected[0], selected[1]);
+      selectedExpr = getMRCARestrictionOfTwoTUs(selected[0], selected[1]);
     } else {
       selectedExpr = createAdditionalClass(
         jsonld,
@@ -255,7 +284,19 @@ function createClassExpressionsForInternals(jsonld, remainingInternals, selected
   return classExprs;
 }
 
-function getExclusionsForExprAndTU(includedExpr, tu) {
+/*
+ * Given an expression that evaluates to an included node and a taxonomic unit,
+ * return an expression for including it and excluding the TU. Note that this
+ * always returns an array.
+ *
+ * When the included expression includes a single taxonomic unit (i.e. is in the
+ * form `includes_TU some [TU]`), then the simple form is adequate. However, when
+ * it's a more complex expression, it's possible that the excluded TU isn't just
+ * sister to this clade but outside of it entirely. In that case, we add another
+ * class expression:
+ *  [includesExpr] and (has_Ancestor some (excludes_TU some [TU]))
+ */
+function getClassExpressionsForExprAndTU(includedExpr, tu) {
   if (!includedExpr) throw new Error('Exclusions require an included expression');
 
   const exprs = [{
@@ -294,16 +335,35 @@ function getExclusionsForExprAndTU(includedExpr, tu) {
   return exprs;
 }
 
+/*
+ * Returns a list of class expressions for a phyloreference that has an expression
+ * for the MRCA of its internal specifiers, but also has one or more external specifiers.
+ *  - jsonld: The JSON-LD form of the Phyloreference from Model 1.0. Mainly used
+ *    for retrieving the '@id' and the specifiers.
+ *  - accumulatedExpr: Initially, an expression that evaluates to the MRCA of all
+ *    internal specifiers, calculated using createClassExpressionsForInternals().
+ *    When we call this method recusively, this expression will incorporate
+ *    representations of external references.
+ *  - remainingExternals: External specifiers that are yet to be incorporated into the
+ *    expressions we are building.
+ *  - selected: External specifiers that have already been selected.
+ *
+ * Our overall algorithm here is:
+ *  1. If we need to add a single remaining external to the accumulated expression,
+ *     we can do that by adding an `excludes_TU` to the expression (and possibly a
+ *     `has_Ancestor` check, see getClassExpressionsForExprAndTU()).
+ *  2. If we need to add more than one remaining external, we select each external
+ *     specifier one at a time. We add the selected specifier to the accumulated
+ *     expression using getClassExpressionsForExprAndTU(), and then call ourselves
+ *     recursively to add the remaining specifiers.
+ *
+ * The goal here is to create expressions for every possible sequence of external
+ * specifiers, so we can account for cases where some external specifiers are closer
+ * to the initial internal-specifier-only expression than others.
+ */
 function createClassExpressionsForExternals(jsonld, accumulatedExpr, remainingExternals, selected) {
-  // When creating a class expression with external specifiers, we can treat the
-  // internal expression as evaluating to a particular set of nodes. Each external
-  // specifier can have one of two relationships with the internal expression node:
-  //  - It could directly excludes_TU the external specifier, or
-  //  - It could have an ancestor that excludes_TU the external specifier.
-  // For the single case, this is straightforward. But when there are multiple
-  // external specifiers, we must use the same recursive algorithm we use to
-  // ensure that we try them out in every possible combination.
-  process.stderr.write(`@id [${jsonld['@id']}] Remaining externals: ${remainingExternals.length}, selected: ${selected.length}\n`);
+  // process.stderr.write(`@id [${jsonld['@id']}] Remaining externals:
+  // ${remainingExternals.length}, selected: ${selected.length}\n`);
 
   // Step 1. If we only have one external remaining, we can provide our two-case example
   // to detect it.
@@ -311,7 +371,7 @@ function createClassExpressionsForExternals(jsonld, accumulatedExpr, remainingEx
   if (remainingExternals.length === 0) {
     throw new Error('Cannot create class expression when no externals remain');
   } else if (remainingExternals.length === 1) {
-    const remainingExternalsExprs = getExclusionsForExprAndTU(
+    const remainingExternalsExprs = getClassExpressionsForExprAndTU(
       accumulatedExpr,
       remainingExternals[0],
       selected.length > 0
@@ -321,15 +381,20 @@ function createClassExpressionsForExternals(jsonld, accumulatedExpr, remainingEx
     // Recurse into remaining externals. Every time we select a single entry,
     // we create a class expression for that.
     remainingExternals.map((newlySelected) => {
-      process.stderr.write(`Selecting new object, remaining now at: ${remainingExternals.filter(i => i !== newlySelected).length}, selected: ${selected.concat([newlySelected]).length}\n`);
+      // process.stderr.write(`Selecting new object, remaining now at:
+      // ${remainingExternals.filter(i => i !== newlySelected).length},
+      // selected: ${selected.concat([newlySelected]).length}\n`);
 
+      // Create a new additional class for the accumulated expression plus the
+      // newly selected external specifier.
       const newlyAccumulatedExpr = createAdditionalClass(
         jsonld,
         jsonld.internalSpecifiers,
         selected.concat([newlySelected]),
-        () => getExclusionsForExprAndTU(accumulatedExpr, newlySelected, selected.length > 0)
+        () => getClassExpressionsForExprAndTU(accumulatedExpr, newlySelected, selected.length > 0)
       );
 
+      // Call ourselves recursively to add the remaining externals.
       return createClassExpressionsForExternals(
         jsonld,
         newlyAccumulatedExpr,
@@ -342,8 +407,6 @@ function createClassExpressionsForExternals(jsonld, accumulatedExpr, remainingEx
       .reduce((acc, val) => acc.concat(val), [])
       .forEach(expr => classExprs.push(expr));
   }
-
-  // console.dir(classExprs, { depth: null })
 
   return classExprs;
 }
