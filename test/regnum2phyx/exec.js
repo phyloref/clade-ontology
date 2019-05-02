c2ce99a17a8ab429e549483c7396750ebc060b25
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-05-02 16:10:07 -0400

    Added tests for regnum2phyx.js conversion.

---
+++ test/regnum2phyx/exec.js
@@ -0,0 +1,78 @@
+/*
+ * exec.js: Tests execution of regnum2phyx.js.
+ */
+
+// Javascript libraries.
+const ChildProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const tmp = require('tmp');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+// Load a JSON file from the file system.
+function loadJSON(filename) {
+    const content = fs.readFileSync(filename, { encoding: 'utf8' });
+    return JSON.parse(content);
+}
+
+describe('Test PHYX files in repository', function () {
+  fs.readdirSync(__dirname + '/examples').forEach(function (filename) {
+    describe(`Processing example Regnum dump: ${filename}`, function () {
+        const filepath = `${__dirname}/examples/${filename}`;
+
+        // We only test '.json' files.
+        if (!filepath.endsWith('.json')) {
+            it.skip('not a JSON file');
+            return;
+        }
+
+        // Create a temporary directory to make files into.
+        const tmpdirname = tmp.dirSync().name;
+
+        // Run phyx2regnum.js on it.
+        const child = ChildProcess.spawnSync(
+            'node',
+            [
+                'regnum2phyx/regnum2phyx.js',
+                filepath,
+                '-o',
+                tmpdirname
+            ],
+            {
+                encoding: 'utf8'
+            }
+        );
+
+        it('could be executed by regnum2phyx.js', function () {
+            expect(child.stderr).to.be.empty;
+            expect(child.stdout).to.match(/^(\d+) Phyx files produced successfully.\n$/);
+            expect(child.status).to.equal(0);
+        });
+
+        // There should be an ./expected/${basename} directory
+        // containing at least one file.
+        const basename = filename.replace(/.json$/i, '');
+        const producedFiles = fs.readdirSync(`${tmpdirname}`);
+        const expectedFiles = fs.readdirSync(`${__dirname}/expected/${basename}`);
+
+        it('should produce the expected files', function () {
+            expect(producedFiles).to.deep.equal(expectedFiles);
+            expect(producedFiles).to.not.be.empty;
+        });
+
+        producedFiles.forEach(function(producedFile) {
+            describe(`Testing produced file ${producedFile}`, function () {
+                const producedPhyx = loadJSON(`${tmpdirname}/${producedFile}`);
+                const expectedPhyx = loadJSON(`${__dirname}/expected/${basename}/${producedFile}`);
+
+                it('should be identical to expected', function () {
+                    expect(producedPhyx).to.deep.equal(expectedPhyx);
+                });
+            });
+        });
+    });
+  });
+});
