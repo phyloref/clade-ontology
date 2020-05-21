029819a6249b9e540c54bbb0d7b23270cb3860aa
Gaurav Vaidya <gaurav@renci.org>
2020-05-21 16:53:56 -0400

    Added a environment flag to choose whether to run slow tests.

--- test/test_phyx.js
+++ test/test_phyx.js
@@ -5,9 +5,12 @@
 const BASE_DIR = 'phyx/';
 
 /*
- * phyx.js uses some code (in particular through phylotree.js) that expects certain
- * Javascript libraries to be loaded via the browser using <script>. To replicate
- * this in Node, we load them and add them to the global object.
+ * The `phyx` folder contains a set of public and private (encrypted)
+ * Phyx files. We can run some fast tests against these:
+ *  - 1. Validate them against the JSON Schema.
+ * We can also run some slow tests:
+ *  - 1. See if we can convert the Phyx file into an ontology without a problem.
+ *  - 2. See if JPhyloRef can validate the ontology without a problem.
  */
 
 // Load phyx.js, our PHYX library.
@@ -20,6 +23,12 @@ const path = require('path');
 const chai = require('chai');
 
 const assert = chai.assert;
+
+/*
+ * Configuration options.
+ */
+/* Flag used to determine whether or not we run the slow tests. */
+const FLAG_SLOW_TESTS = process.env.RUN_SLOW_TESTS || false;
 
 /*
  * Returns a list of PHYX files that we can test in the provided directory.
@@ -81,74 +90,77 @@ describe('Test PHYX files in repository', function () {
       }
 
       // Make sure the produced JSON-LD is not empty.
-      it('produced JSON-LD is not empty', function () {
+      it('produced a non-empty JSON-LD ontology without throwing an exception', function () {
         assert.isNotEmpty(jsonld);
       });
 
       // Test the produced JSON-LD using JPhyloRef.
-      let args = [
-        '-jar', 'jphyloref/jphyloref.jar',
-        'test', '-', '--jsonld',
-      ];
+      if (FLAG_SLOW_TESTS) {
+        let args = [
+          '-jar', `${__dirname}/jphyloref.jar`,
+          'test', '-', '--jsonld',
+        ];
 
-      // Some command line arguments should also be inserted into the command line.
-      // JVM_ARGS should be given to the Java interpreter.
-      if ('JVM_ARGS' in process.env) {
-        args = process.env.JVM_ARGS.split(/\s+/).concat(args);
+        // Some command line arguments should also be inserted into the command line.
+        // JVM_ARGS should be given to the Java interpreter.
+        if ('JVM_ARGS' in process.env) {
+          args = process.env.JVM_ARGS.split(/\s+/).concat(args);
+        }
+
+        // JPHYLOREF_ARGS should be given to JPhyloRef
+        if ('JPHYLOREF_ARGS' in process.env) {
+          args = args.concat(process.env.JPHYLOREF_ARGS.split(/\s+/));
+        }
+
+        // Execute the command line, giving it the JSON-LD on STDIN.
+        const child = ChildProcess.spawnSync('java', args, { input: jsonld });
+
+        // Test whether we can read the test result line from JPhyloRef.
+        // Eventually, we will parse the TAP results directly.
+        const matches = /Testing complete:(\d+) successes, (\d+) failures, (\d+) failures marked TODO, (\d+) skipped./.exec(child.stderr);
+        assert(matches !== null, `Test result line not found in STDERR <${child.stderr}>`);
+        console.log(`For ${filename}: ${matches}`);
+
+        // Test whether we have any failures.
+        it('did not report any failures', function () {
+          const failures = matches[2];
+          assert.equal(failures, 0, `${failures} failures occurred during testing`);
+        });
+
+        describe('test the results of resolution', function () {
+          // Look for TODOs or skipped tests.
+          const successes = matches[1];
+          const todos = matches[3];
+          const skipped = matches[4];
+
+          if (todos > 0) {
+          // TODOs are phyloreferences that we didn't expect to resolve.
+            it.skip(`${todos} phyloreferences were marked as TODO during testing.`);
+            return;
+          }
+
+          if (skipped > 0) {
+          // Skipped phyloreferences are here for historical reasons: JPhyloRef
+          // won't actually recognize any phyloreferences as skipped. This has
+          // been reported as https://github.com/phyloref/jphyloref/issues/40
+            it.skip(`${skipped} phyloreferences were skipped during testing.`);
+            return;
+          }
+
+          // We could have zero failures but also zero successes. A Phyx file
+          // without any failures, TODOs or any successes in the Clade Ontology
+          // should be reported as a failure.
+          it('had at least one success', function () {
+            assert.isAbove(successes, 0, 'No successes occurred during testing');
+          });
+
+          // On the off chance that all of the above made sense but the exit code didn't,
+          // we'll check that here.
+          it('passed testing in JPhyloRef', function () {
+            assert.equal(child.status, 0, 'Exit code from JPhyloRef was not zero');
+          });
+        });
       }
-
-      // JPHYLOREF_ARGS should be given to JPhyloRef
-      if ('JPHYLOREF_ARGS' in process.env) {
-        args = args.concat(process.env.JPHYLOREF_ARGS.split(/\s+/));
-      }
-
-      // Execute the command line, giving it the JSON-LD on STDIN.
-      const child = ChildProcess.spawnSync('java', args, { input: jsonld });
-
-      // Test whether we can read the test result line from JPhyloRef.
-      // Eventually, we will parse the TAP results directly.
-      const matches = /Testing complete:(\d+) successes, (\d+) failures, (\d+) failures marked TODO, (\d+) skipped./.exec(child.stderr);
-      it('produces a valid test result line from JPhyloRef', function () {
-        assert.isNotNull(matches, 'Test result line not found in STDOUT');
-      });
-
-      // Test whether we have any failures.
-      it('did not report any failures', function () {
-        const failures = matches[2];
-        assert.equal(failures, 0, `${failures} failures occurred during testing`);
-      });
-
-      // Look for TODOs or skipped tests.
-      const successes = matches[1];
-      const todos = matches[3];
-      const skipped = matches[4];
-
-      if (todos > 0) {
-        // TODOs are phyloreferences that we didn't expect to resolve.
-        it.skip(`${todos} phyloreferences were marked as TODO during testing.`);
-        return;
-      }
-
-      if (skipped > 0) {
-        // Skipped phyloreferences are here for historical reasons: JPhyloRef
-        // won't actually recognize any phyloreferences as skipped. This has
-        // been reported as https://github.com/phyloref/jphyloref/issues/40
-        it.skip(`${skipped} phyloreferences were skipped during testing.`);
-        return;
-      }
-
-      // We could have zero failures but also zero successes. A Phyx file
-      // without any failures, TODOs or any successes in the Clade Ontology
-      // should be reported as a failure.
-      it('had at least one success', function () {
-        assert.isAbove(successes, 0, 'No successes occurred during testing');
-      });
-
-      // On the off chance that all of the above made sense but the exit code didn't,
-      // we'll check that here.
-      it('passed testing in JPhyloRef', function () {
-        assert.equal(child.status, 0, 'Exit code from JPhyloRef was not zero');
-      });
     });
   });
 });
